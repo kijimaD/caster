10dcd541aaaf254df5c020a7906c8ccb9dc38ff9
Kijima Daigo <norimaking777@gmail.com>
2025-02-22 17:28:35 +0900

    feat: generate webm too

--- webp.sh
+++ webp.sh
@@ -1,21 +1,30 @@
 #!/bin/bash
 set -eux
 
-###########################
-# 今日の録画分をwebpに変換する
-###########################
+#####################
+# 今日の録画分を変換する
+#####################
 
 cd `dirname $0`
 
 DATE=`date +%Y-%m-%d`
 INPUT_DIR=photos/$DATE
-OUTPUT=output/$DATE.webp
+OUTPUT_BASE=output/$DATE
+OUTPUT_WEBP=$OUTPUT_BASE.webp
+OUTPUT_WEBM=$OUTPUT_BASE.webm
 
-ffmpeg \
-    -pattern_type glob -i "$INPUT_DIR/screenshot_*.png" \
-    -r 30 \
-    -q:v 50 -compression_level 6 \
-    -loop 0 \
-    -preset picture \
-    -y \
-    $OUTPUT
+function exec()  {
+    # $1: 出力先
+
+    ffmpeg \
+        -pattern_type glob -i "$INPUT_DIR/screenshot_*.png" \
+        -r 30 \
+        -q:v 50 -compression_level 6 \
+        -loop 0 \
+        -preset picture \
+        -y \
+        $1
+}
+
+exec $OUTPUT_WEBP
+exec $OUTPUT_WEBM
