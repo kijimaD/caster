8bedeb2431f0c1d5233c1e62b42fd3deb4c81a91
Kijima Daigo <norimaking777@gmail.com>
2025-02-22 15:49:54 +0900

    chore: ディレクトリを分ける

--- start.sh
+++ start.sh
@@ -9,6 +9,9 @@ cd `dirname $0`
 
 FONT=Jost-Regular.ttf
 RESOLUTION=960x540
+OUTPUT_DIR=photos/`date +%Y-%m-%d`
+
+mkdir -p $OUTPUT_DIR
 
 ffmpeg \
     -f x11grab \
@@ -16,4 +19,4 @@ ffmpeg \
     -i :0.0 \
     -s $RESOLUTION \
     -vf "drawtext=fontfile=$FONT: text='%{localtime}': fontcolor=white@1: fontsize=128: box=1: boxcolor=0x00000000@0.8: x=7: y=10" \
-    -strftime 1 "screenshot_%Y-%m-%d_%H-%M-%S.png"
+    -strftime 1 "$OUTPUT_DIR/screenshot_%Y-%m-%d_%H-%M-%S.png"
--- webp.sh
+++ webp.sh
@@ -7,10 +7,14 @@ set -eux
 
 cd `dirname $0`
 
+INPUT_DIR=photos/`date +%Y-%m-%d`
+OUTPUT=output/`date +%Y-%m-%d`.webp
+
 ffmpeg \
-    -pattern_type glob -i 'screenshot_*.png' \
+    -pattern_type glob -i "$INPUT_DIR/screenshot_*.png" \
     -r 30 \
     -q:v 50 -compression_level 6 \
     -loop 0 \
     -preset picture \
-    output.webp
+    -y \
+    $OUTPUT
