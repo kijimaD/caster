e967625041b2cbf10d016ae69b3339c68a85783f
Kijima Daigo <norimaking777@gmail.com>
2025-02-24 12:45:37 +0900

    chore: timestamp var

--- record.sh
+++ record.sh
@@ -9,9 +9,10 @@ cd `dirname $0`
 
 font=Jost-Regular.ttf
 resolution=960x540
+timestamp=%Y-%m-%dT%H-%M-%S
 
 # ディレクトリは実行時に決定する
-date=`date +%Y-%m-%dT%H-%M-%S`
+date=`date +$timestamp`
 output_dir=photos/$date
 
 mkdir -p $output_dir
@@ -23,6 +24,6 @@ ffmpeg \
     -s $resolution \
     -vf "drawtext=fontfile=$font: text='%{localtime}': fontcolor=white@1: fontsize=128: box=1: boxcolor=0x00000000@0.8: x=7: y=10" \
     -compression_level 100 \
-    -strftime 1 "$output_dir/screenshot_%Y-%m-%dT%H-%M-%S.png"
+    -strftime 1 "$output_dir/screenshot_$timestamp.png"
 
 ./genvideo.sh $date
