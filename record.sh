4141c396a06a9036d9e8e4ea4bdd6503be4fc87d
Kijima Daigo <norimaking777@gmail.com>
2025-02-24 12:37:21 +0900

    chore: change divider

--- record.sh
+++ record.sh
@@ -11,7 +11,7 @@ font=Jost-Regular.ttf
 resolution=960x540
 
 # ディレクトリは実行時に決定する
-date=`date +%Y-%m-%d_%H-%M-%S`
+date=`date +%Y-%m-%dT%H-%M-%S`
 output_dir=photos/$date
 
 mkdir -p $output_dir
@@ -23,6 +23,6 @@ ffmpeg \
     -s $resolution \
     -vf "drawtext=fontfile=$font: text='%{localtime}': fontcolor=white@1: fontsize=128: box=1: boxcolor=0x00000000@0.8: x=7: y=10" \
     -compression_level 100 \
-    -strftime 1 "$output_dir/screenshot_%Y-%m-%d_%H-%M-%S.png"
+    -strftime 1 "$output_dir/screenshot_%Y-%m-%dT%H-%M-%S.png"
 
 ./genvideo.sh $date
